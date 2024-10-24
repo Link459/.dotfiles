b06adc23838ce0707c8697b7770b57e4c88929fb
Link459 <97700587+Link459@users.noreply.github.com>
2024-10-24 22:42:02 +0200

    migrating alacritty config to the newer alacritty versio

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 cp -r ~/.dotfiles/nvim ~/.config
 cp -r ~/.dotfiles/tmux/ ~/.config
 cp -r ~/.dotfiles/polybar ~/.config
+cp -r ~/.dotfiles/xmonad ~/.config
+
+
+cp ~/.dotfiles/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
 
 cp ~/.dotfiles/conkyrc ~/.conkyrc 
 cp ~/.dotfiles/starship.toml ~/.config
