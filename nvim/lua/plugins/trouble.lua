aff5aebb25d83630c688bc5d631b8c13f38207c9
Link459 <97700587+Link459@users.noreply.github.com>
2024-12-18 18:55:50 +0100

    fixing trouble

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -2,5 +2,6 @@ return {
     'folke/trouble.nvim',
     dependencies = 'nvim-tree/nvim-web-devicons' ,
     cmd = 'Trouble',
-	keys = { { '<leader>t','<cmd>Trouble diagnostics toggle<cr>' } },
+	--keys = { { '<leader>t','<cmd>Trouble diagnostics toggle<cr>' } },
+	keys = { { '<leader>t',function() require('trouble').toggle("diagnostics") end } },
 }
