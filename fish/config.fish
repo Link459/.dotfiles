be763899fc60a40c19efc09c94f1b68c087eb04c
Link459 <97700587+Link459@users.noreply.github.com>
2024-12-21 16:37:41 +0100

    adding cargo binaries to the include path

--- fish/config.fish
+++ fish/config.fish
@@ -10,6 +10,8 @@ alias checkout='git checkout'
 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/link459/.nix-profile/lib"
 export EDITOR=nvim
+#export PATH=$PATH:~/.cargo/bin/
+fish_add_path ~/.cargo/bin/
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
